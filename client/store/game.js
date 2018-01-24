6e973a2e4d0163fbdcc04bfc1203b9a7611760fb
Katy Eng <katye610@gmail.com>
2018-01-24 15:00:45 -0500

    setup react redux store and router

---
+++ client/store/game.js
@@ -0,0 +1,31 @@
+
+/**
+ * ACTION TYPES
+ */
+
+
+/**
+ * INITIAL STATE
+ */
+const defaultUser = {}
+
+/**
+ * ACTION CREATORS
+ */
+
+
+/**
+ * THUNK CREATORS
+ */
+
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultUser, action) {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}
--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import {render} from 'react-dom'
-import {AppContainer} from 'react-hot-loader';
-
-import App from '~/App'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux'
+import App from './client/App'
+import store from './client/store'
 
 function main() {
   render(
     <AppContainer>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </AppContainer>,
     document.getElementById('main'))
 }
 
 main()
 
-module.hot && module.hot.accept('~/App', main)
+module.hot && module.hot.accept('./client/App', main)
