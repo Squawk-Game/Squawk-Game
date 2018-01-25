360153dcb4944a92ac5261cecf41f5df8a3e7f3c
emgordon154 <30507443+emgordon154@users.noreply.github.com>
2018-01-25 16:52:33 -0500

    Update setup.js
    removed secrets

--- fire/setup.js
+++ fire/setup.js
@@ -1,14 +1,8 @@
+require('../secrets')
+
 module.exports = firebase =>
 // Copy and paste this into your JavaScript code to initialize the Firebase SDK.
 // You will also need to load the Firebase SDK.
 // See https://firebase.google.com/docs/web/setup for more details.
-
-firebase.initializeApp({
-  "apiKey": "AIzaSyA2bhm8_n18_VPqP2TQGtOrpAuwyWZZ_4Y",
-  "databaseURL": "https://squawk-868c7.firebaseio.com",
-  "storageBucket": "squawk-868c7.appspot.com",
-  "authDomain": "squawk-868c7.firebaseapp.com",
-  "messagingSenderId": "340493820814",
-  "projectId": "squawk-868c7"
-});
-
+  
+firebase.initializeApp(process.env.INITIALIZEAPP)
