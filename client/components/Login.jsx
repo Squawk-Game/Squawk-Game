5d6ed77114b5d4592d9718c01396a532dbd501f3
Mimi Scott <mimiscott94@gmail.com>
2018-01-25 17:58:48 -0500

    google and email auth now available... goes to firebase auth, not database

---
+++ client/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+//import {database}  from '../../fire'
+import firebase, {firebaseui} from '../../fire'
+
+let uiConfig = {
+  signInSuccessUrl: '/',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  //tosUrl: '<your-tos-url>'
+};
+
+let ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        game: {}
+    }
+  }
+
+  render(){
+    return (
+        <div>
+        <h3>Please Log In</h3>
+      <div id="firebaseui-auth-container"></div>
+      </div>
+    )
+  }
+}
