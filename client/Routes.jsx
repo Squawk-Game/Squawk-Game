5d8485e70decdcf0013508ec6bce04a138d9a439
Katy Eng <katye610@gmail.com>
2018-01-26 16:47:26 -0500

    trying to combine Video and VideoPlayer

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Route, Switch, Router } from 'react-router-dom'
-import Video from './components/Video'
+import VideoPlayer from './components/VideoPlayer'
 import StartGame from './components/StartGame'
 import AddGamePlayers from './components/AddGamePlayers'
 import AudioRecord from './components/AudioRecord'
@@ -15,7 +15,7 @@ const Routes = props => (
 
         <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/video" component={Video}  />
+        <Route path="/video" component={VideoPlayer}  />
         <Route exact path="/" component={StartGame} />
         <Route path="/addusers" component={AddGamePlayers} />
         <Route path="/record" component={AudioRecord} />
