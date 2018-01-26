e51f26618a87fedb59b0f69fd7027f50c66acbe7
Rachel Arkebauer <rarkebauer@gmail.com>
2018-01-26 17:26:17 -0500

    FEATURE a user can authenticate and be updated on state

--- client/components/StartGame.jsx
+++ client/components/StartGame.jsx
@@ -1,40 +1,37 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
-import {database} from '../../fire'
+import {database, auth} from '../../fire'
 import AddGamePlayers from './AddGamePlayers'
 import Login from './Login'
+
 
 export default class StartGame extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        game: {},
-        users: {},
-        judge: '',
-        audio: [],
-        video: '',
-        me: {}
+       users: null
     }
   }
   componentDidMount(){
-    console.log(this.state)
     let gamesRef = database.ref('games')
     gamesRef.on("value", function(snapshot) {
         console.log(snapshot.val()[1]);
       }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
       });
+
   }
   render(){
     return (
       <div>
         <h1>SQUAWK!</h1>
-        {this.state.me.name &&
+        {/* {this.state.me.name &&
           <Link to={'/addusers'}>START</Link>
+        } */}
+        {
+          !this.state.users && <Login />
         }
-        {!this.state.me.name &&
-          <Login />
-        }
+
         <br />
       </div>
     )
