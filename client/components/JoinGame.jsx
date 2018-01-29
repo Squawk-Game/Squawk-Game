8af878913909f05072d10666332c80b1032afeac
Rachel Arkebauer <rarkebauer@gmail.com>
2018-01-29 16:20:00 -0500

    created join game component

---
+++ client/components/JoinGame.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import firebase, {firebaseui, auth, database} from '~/fire'
+
+const JoinGame = (props) => {
+  //console.log(props)
+  return (
+    <h1>JOIN A GAME</h1>
+  )
+}
+
+export default JoinGame
