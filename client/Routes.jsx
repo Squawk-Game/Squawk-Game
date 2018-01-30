46fa17aa2767a47de064347f6127661603272747
Katy Eng <katye610@gmail.com>
2018-01-30 16:57:21 -0500

    made Game component and changed firebase format

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -5,6 +5,7 @@ import StartGame from './components/StartGame'
 import AddGamePlayers from './components/AddGamePlayers'
 import AudioRecord from './components/AudioRecord'
 import HostVideo from './components/HostVideo'
+import Game from './components/Game'
 import history from './history'
 import JoinGame from './components/JoinGame'
 /**
@@ -17,6 +18,7 @@ const Routes = props => (
 
         <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/game/:gameId" component={Game}  />
         <Route path="/player-video" component={PlayerVideo}  />
         <Route exact path="/" component={StartGame} />
         <Route path="/addusers" component={AddGamePlayers} />
