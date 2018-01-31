37384adb8bf57ac748c422135d5af7071b48ad93
Rachel Arkebauer <rarkebauer@gmail.com>
2018-01-31 16:55:53 -0500

    started Dumb video

--- client/components/Game.js
+++ client/components/Game.js
@@ -72,16 +72,21 @@ export default class Game extends Component {
 
         {this.state.gameState === WAITING_TO_START && <WaitingRoom isJudge={this.state.playerRole === 'JUDGE' ? true : false} /> }
 
+        {/* IF STATE IS WINNER_SENT */}
+        {this.state.gameState === WINNER_SENT && <WinnerPage gameKey={this.state.gameId} />}
+
+        {/* IF STATE IS GAME_CLOSED push to home for now and destroy game including destroying player audio and changing in game to false */}
+        {this.state.gameState === GAME_CLOSED && history.push(`/`)}
 
         {this.state.gameState === WINNER_SENT && <WinnerPage gameKey={this.state.gameId} />}
 
-        {(this.state.gameState === VIDEO_SENT 
+        {(this.state.gameState === VIDEO_SENT
           || this.state.gameState === WAITING_FOR_AUDIO)
-          && this.state.playerRole === 'JUDGE' 
+          && this.state.playerRole === 'JUDGE'
           && <HostVideo gameKey={this.state.gameId}/>}
-        {(this.state.gameState === VIDEO_SENT 
+        {(this.state.gameState === VIDEO_SENT
           || this.state.gameState === WAITING_FOR_AUDIO)
-          && this.state.playerRole === 'PLAYER' 
+          && this.state.playerRole === 'PLAYER'
           && <PlayerVideo gameKey={this.state.gameId}/>}
           {console.log('this is here for ocmmit probs')}
 
@@ -89,3 +94,7 @@ export default class Game extends Component {
     )
   }
 }
+
+//audio on game will have push key of userId as key and audio path as file
+//audio gets associated with user
+
--- client/components/WinnerPage.jsx
+++ client/components/WinnerPage.jsx
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 
 import {database, auth} from '../../fire'
-import HostVideo from './HostVideo'
+import DumbVideo from './DumbVideo'
 
 
 export default class WinnerPage extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      audio: null,
+      video: null
+    }
+  }
 
+  componentDidMount() {
+    Promise.all([
+      database.ref(`games/${this.props.gameKey}`)
+    ])
   }
 
   render(){
     return (
       <div>
         IN THE WINNER PAGE
-        <HostVideo /> {/* need to pass in video and audio props of winner */}
+        <DumbVideo audio={this.state.audio} video={this.state.video}/> {/* need to pass in video and audio props of winner */}
       </div>
     )
   }
