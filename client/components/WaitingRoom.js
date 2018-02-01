9840850b8acb7e2af0a5bfc1e714dbe8a2787d32
Katy Eng <katye610@gmail.com>
2018-02-01 11:18:32 -0500

    able to render players in WaitingRoom as they join a game

--- client/components/WaitingRoom.js
+++ client/components/WaitingRoom.js
@@ -4,40 +4,54 @@ import { database, auth } from '../../fire'
 
 
 export default class WaitingRoom extends Component {
- constructor(props) {
-   super(props)
-   this.state = {
-     playersInGame: null
-   }
- }
+  constructor(props) {
+    super(props)
+    this.state = {
+      playersInGame: null
+    }
+  }
 
- componentDidMount() {
-   let self = this
-   let gameRef = database.ref(`games/${this.props.gameKey}`)
+  componentDidMount() {
+    let self = this
+    let gameRef = database.ref(`games/${this.props.gameKey}`)
+    gameRef.once("value")
+      .then(function (snap) {
+        self.setState({ playersInGame: snap.val().players })
+      })
+      .then(function () {
+        gameRef.on('child_changed', (snap) => {
+          if (snap.key === 'players') {
+            self.setState({ playersInGame: snap.val() })
+          }
+        })
+      })
+  }
 
-   gameRef.once("value")
-   .then(function(snap){
-     self.setState({playersInGame: snap.val().players})
-   }).then(function() {
-     gameRef.on('child_changed', (snap) => {
-       if (snap.key === 'players'){
-         self.setState({playersInGame: snap.val()})
-       }
-     })
-   })
- }
-
- render() {
-
-if(this.state.playersInGame){
-  console.log("state inWaitng", this.state.playersInGame)
-}
-   return (
-     <div>
-       <h2>Welcome To Game #{this.props.code}</h2>
-
-     </div>
-   )
- }
+  render() {
+    if (!this.state.playersInGame) {
+      //Put spinning wheel here
+      return <h1>Loading Game...</h1>
+    } else {
+      let arrPlayers = []
+      for (let key in this.state.playersInGame) {
+        arrPlayers.unshift(this.state.playersInGame[key])
+      }
+      console.log("Heres the arrPlayers", arrPlayers)
+      return (
+        <div>
+          <h1>Welcome To Game #{this.props.code}</h1>
+          <h2>Judge: {arrPlayers[0]}</h2>
+          <h3>Squawkers:</h3>
+          {arrPlayers.slice(1).map(player => {
+            return (
+              <div key={player}>
+                <li>{player}</li>
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
+  }
 }
 
