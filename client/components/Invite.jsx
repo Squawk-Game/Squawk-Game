c8dc4bb383e8bf4145884fbb497f8b481b8e6aa7
Katy Eng <katye610@gmail.com>
2018-02-01 15:19:06 -0500

    sending email invitations with code

--- client/components/Invite.jsx
+++ client/components/Invite.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {database} from '../../fire'
+import { database } from '../../fire'
 //you are here because you are a judge and want to add players to your new game
 export default class Invite extends Component {
   constructor(props) {
@@ -9,15 +9,29 @@ export default class Invite extends Component {
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick(event){
+  handleClick(event) {
+    let self = this
     event.preventDefault()
-    database.ref(`games/${this.props.gameKey}`).update({judgeState: 'WAITING_TO_START'})
+    let emailArr = event.target.emails.value.split(',').map(email => email.trim())
+    emailArr.forEach(function(email) {
+      emailjs.send("ghsquawk", "squawk_invitation", {"recipient":email,"code": self.props.code})
+      .then(function(response) {
+       console.log("Code sent!", response.status, response.text)
+      }, function(err) {
+       console.log("FAILED. error=", err);
+      })
+    })
+    database.ref(`games/${this.props.gameKey}`).update({ judgeState: 'WAITING_TO_START' })
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <button onClick={this.handleClick}>Sent invites</button>
+      <h3>Enter invitation recipients' email addresses separated with a comma:</h3>
+        <form onSubmit={this.handleClick}>
+          <input type="text" name="emails" placeholder="ex1@ex.com, ex2@ex.com" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
