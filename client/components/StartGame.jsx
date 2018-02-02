2bcc0fb06c4aaae780c92281d685a73d677457f7
Mimi Scott <mimiscott94@gmail.com>
2018-02-02 16:41:25 -0500

    janky intro chicken

--- client/components/StartGame.jsx
+++ client/components/StartGame.jsx
@@ -3,29 +3,44 @@ import {Link} from 'react-router-dom'
 import {database, auth} from '../../fire'
 import AddGamePlayers from './AddGamePlayers'
 import Login from './Login'
+import { setTimeout } from 'timers';
 
 
 export default class StartGame extends Component {
   constructor(props) {
     super(props)
     this.state = {
-       users: null
+       users: null,
+       gif: '~/public/giphy.mp4',
+       timing: 2
     }
   }
   componentDidMount(){
-    // let gamesRef = database.ref('games')
-    // gamesRef.on("value", function(snapshot) {
-    //     console.log(snapshot.val()[1]);
-    //   }, function (errorObject) {
-    //     console.log("The read failed: " + errorObject.code);
-    //   });
+    setTimeout(() => {
+
+      if(this.state.timing === 2 ) this.setState({timing: 1})
+      if(this.state.timing === 1) this.setState({timing: 0})
+    }
+    , 5000)
   }
   render(){
+    console.log('!!',this.state.timing)
     return (
       <div>
+      {
+        this.state.timing &&
+        <div>
+          <iframe src="https://giphy.com/embed/l4pTiBbkdDYmsZ7Gw" width="320" height="480" frameBorder="0"  allowFullScreen></iframe><p><a href="https://giphy.com/gifs/l4pTiBbkdDYmsZ7Gw"></a></p>
+        </div>
+      }
+      {
+        !this.state.timing &&
+        <div>
         <h1>SQUAWK!</h1>
         <Login />
         <br />
+        </div>
+      }
       </div>
     )
   }
