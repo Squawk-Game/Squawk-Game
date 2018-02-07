076026256b5c70dee4a2548d68e7f7dd62ea71d8
Mimi Scott <mimiscott94@gmail.com>
2018-02-07 14:55:25 -0500

    game new loops around without looping video;

--- client/components/WinnerPage.jsx
+++ client/components/WinnerPage.jsx
@@ -4,30 +4,34 @@ import {database, auth} from '../../fire'
 import DumbVideo from './DumbVideo'
 
 import sizeMe from 'react-sizeme'
-import Confetti from 'react-confetti'
+//import Confetti from 'react-confetti'
 import PropTypes from 'prop-types'
 
-const DimensionedWinnerPage = sizeMe({
-  monitorHeight: true,
-  monitorWidth: true,
-})(class WinnerPage extends React.PureComponent {
-  static propTypes = {
-    size: PropTypes.shape({
-      width: PropTypes.number,
-      height: PropTypes.number,
-    }),
-  }
+import Confetti from 'react-dom-confetti'
+
+const config = {
+  angle: 90,
+  spread: 250,
+  startVelocity: 30,
+  elementCount: 500,
+  decay: 0.95
+}
+
+class WinnerPage extends React.PureComponent {
     constructor(props) {
     super(props)
     this.state = {
       audio: null,
       video: null,
-      redirect: false
+      redirect: false,
+      confetti: false
     }
   }
 
     componentDidMount() {
+      console.log('width is ', this.state.proptypes)
     let self = this
+    self.setState({confetti: true})
     Promise.all([
       database.ref(`games/${this.props.gameKey}/video`),
       //audio needs to be the player's id bc that's the name of their svideo
@@ -50,16 +54,70 @@ const DimensionedWinnerPage = sizeMe({
     render(){
     return (
       <div>
-        <div className="winner-content" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-          <Confetti {...this.props.size} />
-          </div>
+          <Confetti active={ this.state.confetti } config={ config } />
           <DumbVideo audio={this.state.audio} video={this.state.video} loops={9} gameKey={this.props.gameKey} />
       </div>
     )
   }
-})
+}
 
-export default DimensionedWinnerPage
+export default WinnerPage
+
+// const DimensionedWinnerPage = sizeMe({
+//   monitorHeight: true,
+//   monitorWidth: true,
+// })(class WinnerPage extends React.PureComponent {
+//   static propTypes = {
+//     size: PropTypes.shape({
+//       width: PropTypes.number,
+//       height: PropTypes.number,
+//     }),
+//   }
+//     constructor(props) {
+//     super(props)
+//     this.state = {
+//       audio: null,
+//       video: null,
+//       redirect: false,
+//       proptypes: this.props.width
+//     }
+//   }
+
+//     componentDidMount() {
+//       console.log('width is ', this.state.proptypes)
+//     let self = this
+//     Promise.all([
+//       database.ref(`games/${this.props.gameKey}/video`),
+//       //audio needs to be the player's id bc that's the name of their svideo
+//       database.ref(`games/${this.props.gameKey}/winningAudio`)
+//     ]).then((refs) => {
+//       let videoRef = refs[0]
+//       let winningAudioRef = refs[1]
+//       videoRef.on('value', (snap) => {
+//         self.setState({ video: snap.val() })
+//       })
+//       winningAudioRef.on('value', (snap) => {
+//         self.setState({ audio: snap.val()})
+//       })
+//       .then(() => {
+//         let thisUserAudioId = self.state.audio.indexOf()
+//       })
+//     })
+//   }
+
+//     render(){
+//     return (
+//       <div>
+//         <div className="winner-content" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 99 }}>
+//           <Confetti {...this.props.size} />
+//           </div>
+//           <DumbVideo audio={this.state.audio} video={this.state.video} loops={9} gameKey={this.props.gameKey} />
+//       </div>
+//     )
+//   }
+// })
+
+// export default DimensionedWinnerPage
 
 // export default class WinnerPage extends Component {
 //   constructor(props) {
